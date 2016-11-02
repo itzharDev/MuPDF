c33debf68176fcdac7650e79ee2a605d4f9cb4c5
Itzhar Mordechai <itzhar.dev@gmail.com>
2016-11-02 09:07:01 +0200

    init commit

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/bin/AndroidManifest.xml : \
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/AndroidManifest.xml \
---
+++ bin/MuPDF-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/bin/MuPDF-debug-unaligned.apk : \
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/bin/MuPDF.ap_ \
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/bin/classes.dex \
+/Users/itzharmordechai/Desktop/mupdf/platform/android/viewer/libs/armeabi-v7a/libmupdf_java.so \
